9971ae1142c6e97f40216a2ca42203201e9c1737
sokrasins <sokrasins@gmail.com>
2025-03-20 23:34:00 -0400

    CHange main around

--- fan_control/main/main.c
+++ fan_control/main/main.c
@@ -8,16 +8,28 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 
 #define LONG_PRESS_TIME 1000U //ms
 #define LOOP_TIME       100U  //ms
+
+#define BUTTON_QUEUE_DEPTH 10U
+
+typedef enum {
+    APP_FAN_ON,
+    APP_FAN_OFF,
+} app_state_t;
 
 // Local functions
 void button_cb(button_handle_t handle, button_state_t state);
 void advance_fan_speed(fan_speed_t *speed);
 
 // App state
-fan_speed_t speed = FAN_SPEED_OFF;
+fan_speed_t speed = FAN_SPEED_1; // TODO: store in NVS
+app_state_t app_state = APP_FAN_OFF;
+
+// Queue for button change events
+QueueHandle_t button_queue = NULL;
 
 // Button state
 bool pushed = false;
@@ -30,6 +42,9 @@ button_handle_t *button = NULL;
 void app_main(void) 
 {
     status_t status;
+
+    // Set up queue
+    button_queue = xQueueCreate(BUTTON_QUEUE_DEPTH, sizeof(uint8_t));
 
     // Set up the fan
     fan = fan_init(FAN_CHAN_1, BOARD_FAN_OUT_PIN);
@@ -57,49 +72,79 @@ void app_main(void)
 
     while (1)
     {
-        // Check for a long button press here
+        uint8_t button_state;
+        if (xQueueReceive(button_queue, &button_state, pdMS_TO_TICKS(LOOP_TIME))) 
+        {
+            switch (app_state)
+            {
+                case APP_FAN_OFF:
+                    if (button_state == BUTTON_STATE_OFF)
+                    {
+                        // If the fan is off, then turn it on as soon as the 
+                        // button is pressed
+                        INFO("Turning Fan ON with speed: %d", speed);
+                        status = fan_set_speed(fan, speed);
+                        app_state = APP_FAN_ON;
+                    }
+                    break;
+
+                case APP_FAN_ON:
+                    if(button_state == BUTTON_STATE_OFF)
+                    {
+                        // If button push has started, then record the time the 
+                        // button press was started: we need to figure out if 
+                        // its a short or long press
+                        pushed = true;
+                        push_time = millis();
+                    }
+                    else if (button_state == BUTTON_STATE_ON)
+                    {
+                        // If button push hasn't already been consumed (i.e. 
+                        // long press)
+                        if (pushed)
+                        {
+                            // Consume button push
+                            pushed = false;
+
+                            // Change fan speed and set
+                            advance_fan_speed(&speed);
+                            status = fan_set_speed(fan, speed);
+                            if (status != STATUS_OK)
+                            {
+                                ERROR("ERROR setting fan speed: %u", (int)status);
+                            }
+                            INFO("New fan speed: %d", speed);
+                        }
+                    }
+                    break;
+            }
+        }     
+
+        // If button has been pressed more than the long press time, then it's 
+        // a long press
         if (pushed && (millis() - push_time) >= LONG_PRESS_TIME)
         {
-            INFO("Long button press detected, shutting off fan");
+            // Consume button push
             pushed = false;
-            speed = FAN_SPEED_OFF;
+            app_state = APP_FAN_OFF;
+            INFO("Turning fan OFF");
 
-            status = fan_set_speed(fan, speed);
+            // Set speed off
+            status = fan_set_speed(fan, FAN_SPEED_OFF);
             if (status != STATUS_OK)
             {
                 ERROR("ERROR setting fan speed: %u", (int)status);
             }
         }
 
-        vTaskDelay(pdMS_TO_TICKS(LOOP_TIME));
     }
 }
 
 void button_cb(button_handle_t handle, button_state_t state) 
 {
-    status_t status;
-    if(state == BUTTON_STATE_OFF)
-    {
-        pushed = true;
-        push_time = millis();
-    }
-    else if (state == BUTTON_STATE_ON)
-    {
-        if (pushed)
-        {
-            pushed = false;
-            // Advance fan speed
-            advance_fan_speed(&speed);
-            INFO("new fan speed: %d", speed);
-        
-            // Set the new fan speed
-            status = fan_set_speed(fan, speed);
-            if (status != STATUS_OK)
-            {
-                ERROR("ERROR setting fan speed: %u", (int)status);
-            }
-        }
-    }
+    // Send button event to our state machine
+    uint8_t byte_state = state & 0xFF;
+    xQueueSend(button_queue, &byte_state, 0);
 }
 
 void advance_fan_speed(fan_speed_t *speed) 
@@ -108,7 +153,7 @@ void advance_fan_speed(fan_speed_t *speed)
     {
         case FAN_SPEED_ON:
             // Go back to the beginning
-            *speed = FAN_SPEED_OFF;
+            *speed = FAN_SPEED_1;
             break;
 
         default:
